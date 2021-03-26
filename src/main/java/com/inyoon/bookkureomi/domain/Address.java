408365d42b04ad55315b5b1d968c88e11fc3e456
innjie <innjie@naver.com>
2021-03-27 02:39:36 +0900

    [update] DTO files

--- src/main/java/com/inyoon/bookkureomi/domain/Address.java
+++ src/main/java/com/inyoon/bookkureomi/domain/Address.java
@@ -5,6 +5,8 @@ import lombok.Data;
 public @Data
 class Address {
     private int addrNo;
+    private int userNo;
     private String address;
+    private String aName;
     private int zipcode;
 }
