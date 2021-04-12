59287176692234975746e7ad02bf18208fe81152
im57 <eovhehd1986@gmail.com>
2021-04-12 21:46:58 +0900

    [add] insert, update sale

--- src/main/java/com/inyoon/bookkureomi/sale/SaleController.java
+++ src/main/java/com/inyoon/bookkureomi/sale/SaleController.java
@@ -51,7 +51,7 @@ public class SaleController{
 	
 	@ApiOperation(value="중고거래 목록", notes="중고거래 전체 목록을 보여준다.")
 	@ResponseBody //@RestController 시 생략 가능
-	@GetMapping("/sale/saleList")
+	@GetMapping("/sale/list")
 	public Map<String, Object> listSale() {
 
 		List<Sale> saleList = new ArrayList<>();	
@@ -65,7 +65,7 @@ public class SaleController{
 	
 	@ApiOperation(value="중고거래 찾기", notes="제목/장르로 중고거래를 찾는다.")
 	@ResponseBody //@RestController 시 생략 가능
-	@GetMapping("/sale/findSale")
+	@GetMapping("/sale/find")
 	public Map<String, Object> findSale(
 			@RequestParam("title") String title,
 			@RequestParam("genre") String genre) {
@@ -86,8 +86,8 @@ public class SaleController{
 	
 	@ApiOperation(value="중고 책 상세 보기", notes="판매되는 중고 책을 상세히 본다.")
 	@ResponseBody //@RestController 시 생략 가능
-	@GetMapping("/sale/getSale")
-	public Map<String, Object> getSale(
+	@GetMapping("/sale/detail")
+	public Map<String, Object> detailSale(
 				@RequestParam("saleNo") int saleNo) throws Exception {
 
 		Sale sale = new Sale();
@@ -102,14 +102,14 @@ public class SaleController{
 	
 	@ApiOperation(value="판매 책 등록 ", notes="중고 책을 판매한다.")
 	@ResponseBody //@RestController 시 생략 가능
-	@PostMapping("/sale/saleBook")
-	public Map<String, Object> saleBook(
+	@PostMapping("/sale/create")
+	public Map<String, Object> createSale(
 			@RequestParam("title") String title,
 			@RequestParam("publisher") String publisher,
 			@RequestParam("salePrice") int salePrice,
 			@RequestParam("info") String info,
 			@RequestParam("costPrice") int costPrice,
-			//@RequestParam("image") String image,
+			@RequestParam("image") String image,
 			@RequestParam("userNo") int userNo,
 			@RequestParam("author") String author,
 			@RequestParam("genreType") String genreType) throws Exception {
@@ -120,7 +120,7 @@ public class SaleController{
 		Genre genre = genreService.getGenreByName(genreType);
 		
 		sale.setSaleNo(saleService.getSaleNo());
-		//sale.setImage(image);
+		sale.setImage(image);
 		sale.setTitle(title);
 		sale.setPublisher(publisher);
 		sale.setSalePrice(salePrice);
@@ -142,7 +142,7 @@ public class SaleController{
 	
 	@ApiOperation(value="판매 등록 책 수정 ", notes="판매하는 중고 책을 수정한다.")
 	@ResponseBody //@RestController 시 생략 가능
-	@PostMapping("/sale/updateSale")
+	@PutMapping("/sale/update")
 	public Map<String, Object> updateSale(
 			@RequestParam("saleNo") String saleNo,
 			@RequestParam("image") String image,
@@ -162,6 +162,20 @@ public class SaleController{
 		
         return map;
 	}
+	
+	
+	
+	@ApiOperation(value="나의 판매 화면 이동", notes="나의 판매 화면으로 이동한다.")
+	@GetMapping("/mypage/sale/view")
+    public String viewMySale(Model model) {	
+        return "sale/mySaleList";
+    }
+	
+	@ApiOperation(value="나의 판매 목록", notes="나의 판매 전체 목록을 보여준다.")
+	@GetMapping("/mypage/sale/list")
+    public String listMySale(Model model) {	
+        return "sale/mySaleList";
+    }
 	
 /*
 	//SaleCommand
--- src/main/resources/static/js/sale/sale.js
+++ src/main/resources/static/js/sale/sale.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	listSaleBook();
+	listSale();
 });
 
 //판매 중고 서적 나열
-function listSaleBook(){
+function listSale(){
 	$.ajax({
-		url: "/book/sale/saleList", 
+		url: "/book/sale/list", 
 		method: 'GET',
 	    dataType: "json",
 		data: {
@@ -20,7 +20,9 @@ function listSaleBook(){
 		var sale;
 		
 		for(var i=0; i<data.saleList.length; i++){
-			if(i%3 == 0 && i != 0){
+			if(i%3 == 0){
+				result += "</tr><tr>";
+			} else if(i == 0){
 				result += "<tr>";
 			}
 			
@@ -38,22 +40,8 @@ function listSaleBook(){
 						+ "<li class=\"table-list-content-list-style\">" + data.saleList[i].author + " / " + data.saleList[i].publisher + "</li>"
 						+ "<li class=\"table-list-content-list-style\"> \\" + data.saleList[i].costPrice + " -> \\" + data.saleList[i].salePrice + "</li>"
 						+ "<li class=\"table-list-content-list-style\">" + data.saleList[i].state + "</li>"
-						+ "<li class=\"table-list-content-btn-style\"><button type=\"button\" id=\"btnSale"+ data.saleList[i].saleNo +"\" class=\"view-btn\" onClick=\"getSale("+ data.saleList[i].saleNo +")\">상세보기</button><li>"
+						+ "<li class=\"table-list-content-btn-style\"><button type=\"button\" id=\"btnSale"+ data.saleList[i].saleNo +"\" class=\"view-btn\" onClick=\"detailSale("+ data.saleList[i].saleNo +")\">상세보기</button><li>"
 						+ "</ul></li>";
-			
-/*			sale = "<li><h3>" + data.saleList[i].saleNo + "</h3></li>"
-					+"<li><img src=\"/images" + data.saleList[i].image + "\" /></li>"
-					+"<li>" + data.saleList[i].publisher + "</li>"
-					+"<li>" + data.saleList[i].author + "</li>"
-					+"<li>" + data.saleList[i].title + "</li>"
-					+"<li>" + data.saleList[i].costPrice + "</li>"
-					+"<li>" + data.saleList[i].salePrice + "</li>"
-					+"<li>" + data.saleList[i].info + "</li>"
-				//	+"<li>" + data.saleList[i].user.name + "</li>"
-					+"<li>" + data.saleList[i].regiDate + "</li>"
-					+"<li>" + data.saleList[i].genre.genreType + "</li>"
-					+"<li>" + data.saleList[i].state + "</li>";
-			result += sale;*/
 			
 			result += "</ul></td>";
 			
@@ -63,7 +51,7 @@ function listSaleBook(){
 				result += "<td></td>";
 			}
 			
-			if(i%3==0  && i != 0){
+			if(i == data.saleList.length-1){
 				result += "</tr>";
 			}
 		}
@@ -77,7 +65,7 @@ function listSaleBook(){
 }
 
 //판매 중고 서적 찾기
-function findSaleBook(){
+function findSale(){
 	var title = '';
 	var genre = '';
 	var type = $("#findType").val();
@@ -89,7 +77,7 @@ function findSaleBook(){
 	}
 		
 	$.ajax({
-		url: "/book/sale/findSale", 
+		url: "/book/sale/find", 
 		method: 'GET',
 	    dataType: "json",
 		data: {
@@ -107,7 +95,9 @@ function findSaleBook(){
 					+ "<colgroup><col width=\"33.333333%\" /><col width=\"33.333333%\" /><col width=\"33.333333%\" /></colgroup><tbody>";
 							
 			for(var i=0; i<data.saleList.length; i++){
-				if(i%3 == 0 && i != 0){
+				if(i%3 == 0){
+					result += "</tr><tr>";
+				} else if(i == 0){
 					result += "<tr>";
 				}
 				
@@ -125,22 +115,8 @@ function findSaleBook(){
 							+ "<li class=\"table-list-content-list-style\">" + data.saleList[i].author + " / " + data.saleList[i].publisher + "</li>"
 							+ "<li class=\"table-list-content-list-style\"> \\" + data.saleList[i].costPrice + " -> \\" + data.saleList[i].salePrice + "</li>"
 							+ "<li class=\"table-list-content-list-style\">" + data.saleList[i].state + "</li>"
-							+ "<li class=\"table-list-content-btn-style\"><button type=\"button\" id=\"btnSale"+ data.saleList[i].saleNo +"\" class=\"view-btn\" onClick=\"getSale("+ data.saleList[i].saleNo +")\">상세보기</button><li>"
+							+ "<li class=\"table-list-content-btn-style\"><button type=\"button\" id=\"btnSale"+ data.saleList[i].saleNo +"\" class=\"view-btn\" onClick=\"detailSale("+ data.saleList[i].saleNo +")\">상세보기</button><li>"
 							+ "</ul></li>";
-				
-	/*			sale = "<li><h3>" + data.saleList[i].saleNo + "</h3></li>"
-						+"<li><img src=\"/images" + data.saleList[i].image + "\" /></li>"
-						+"<li>" + data.saleList[i].publisher + "</li>"
-						+"<li>" + data.saleList[i].author + "</li>"
-						+"<li>" + data.saleList[i].title + "</li>"
-						+"<li>" + data.saleList[i].costPrice + "</li>"
-						+"<li>" + data.saleList[i].salePrice + "</li>"
-						+"<li>" + data.saleList[i].info + "</li>"
-					//	+"<li>" + data.saleList[i].user.name + "</li>"
-						+"<li>" + data.saleList[i].regiDate + "</li>"
-						+"<li>" + data.saleList[i].genre.genreType + "</li>"
-						+"<li>" + data.saleList[i].state + "</li>";
-				result += sale;*/
 				
 				result += "</ul></td>";
 				
@@ -150,7 +126,7 @@ function findSaleBook(){
 					result += "<td></td>";
 				}
 				
-				if(i%3==0  && i != 0){
+				if(i == data.saleList.length-1){
 					result += "</tr>";
 				}
 			}
@@ -167,16 +143,18 @@ function findSaleBook(){
 }
 
 //판매 중고서적 상세보기
-function getSale(saleNo) {	
+function detailSale(saleNo) {	
+	closeCreatePopup();
+
 	$.ajax({
-		url: "/book/sale/getSale", 
+		url: "/book/sale/detail", 
 		method: 'GET',
 	    dataType: "json",
 		data: {
 			saleNo : saleNo
 		}
 	}).done(function( data ) {
-		$("#pop-sale-view").css("display", "block");
+		$("#pop-sale-detail").css("display", "block");
 		
 		$("#viewImage").attr("src", data.sale.image);
 		
@@ -203,30 +181,77 @@ function getSale(saleNo) {
     });
 }
 
-function closeView() {
-	$("#pop-sale-view").css("display", "none");
-	$("#pop-sale-insert").css("display", "none");
+function closeDetailPopup() {
+	$("#pop-sale-detail").css("display", "none");
 }
 
+function closeCreatePopup() {
+	$("#pop-sale-create").css("display", "none");
+}
+
+function setDefault() {
+	$("#insertTitle").val('');
+	$("#insertPublisher").val('');
+	$("#insertAuthor").val('');
+	$("#insertCostPrice").val('');
+	$("#insertSalePrice").val('');
+	$("#insertRegiDate").val('');
+	$("#insertGenreType").val('');
+	$("#insertState").val('');
+	$("#insertInfo").val('');
+}
 
 //중고 서적 판매
-function saleBookForm(){
+function createSaleForm(){
 	setDefault();
-	$("#pop-sale-insert").css("display", "block");
+	closeDetailPopup();
+	$("#pop-sale-create").css("display", "block");
 }
-function saleBook() {
+function createSale() {	
 	var publisher = $("#insertPublisher").val();
 	var title = $("#insertTitle").val();
 	var costPrice = $("#insertCostPrice").val();
 	var userNo = 1;
 	var author = $("#insertAuthor").val();
 	var genreType = $("#insertGenreType").val();
-	//var image = ''; //폼처리?
+	var image = '/images/sale/0.png'; //폼처리?
 	var salePrice = $("#insertSalePrice").val();
-	var info = $("#insertInfo").val();
-	
+	var info = $("#insertInfo").val();	
+
+	var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    
+    if(title == '' || title == undefined){
+    	alert("제목을 입력하세요.");
+    	return;
+    } else if(publisher == '' || publisher == undefined){
+    	alert("출판사를 입력하세요.");
+    	return;
+    } else if(author == '' || author == undefined){
+    	alert("저자를 입력하세요.");
+    	return;
+    } else if(costPrice == '' || costPrice == undefined){
+    	alert("원가를 입력하세요.");
+    	return;
+    } else if(!Number.isInteger(parseInt(costPrice))){
+    	alert("원가는 숫자로 입력하세요.");
+    	return;
+    } else if(salePrice == '' || salePrice == undefined){
+    	alert("판매가를 입력하세요.");
+    	return;
+    } else if(!Number.isInteger(parseInt(salePrice))){
+    	alert("판매가는 숫자로 입력하세요.");
+    	return;
+    } else if(genreType == '' || genreType == undefined){
+    	alert("장르를 입력하세요.");
+    	return;
+    } else if(info == '' || info == undefined){
+    	alert("정보를 입력하세요.");
+    	return;
+    }
+    
 	$.ajax({
-		url: "/book/sale/saleBook", 
+		url: "/book/sale/create", 
 		method: 'POST',
 	    dataType: "json",
 		data: {
@@ -235,16 +260,22 @@ function saleBook() {
 			salePrice : salePrice,
 			info : info, 
 			costPrice : costPrice,
-			//image : image,
+			image : image,
 			userNo : userNo,
 			author : author,
 			genreType : genreType
-		}
+		},
+		beforeSend : function(xhr){   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+            xhr.setRequestHeader(header, token);
+        }
 	}).done(function( data ) {
-		if(data.result == success){
+		if(data.result == 'success'){
 			window.alert("중고 책을 등록하였습니다.");
 	        
-			getSale();
+			$("#pop-sale-insert").css("display", "none");
+			
+			listSale();
+			detailSale(data.saleNo);
 		}
 	})
     .fail( function( textStatus ) {
@@ -253,14 +284,28 @@ function saleBook() {
 }
 
 //수정
-function updateBook() {
+function updateSale() {	
 	var saleNo = $("#viewSaleNo").val();
 	var image = $("#viewImage").attr("src"); //폼처리?
 	var salePrice = $("#viewSalePrice").val();
 	var info = $("#viewInfo").val();
 	
+	var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+	
+    if(salePrice == '' || salePrice == undefined){
+    	alert("판매가를 입력하세요.");
+    	return;
+    } else if(!Number.isInteger(parseInt(salePrice))){
+    	alert("판매가는 숫자로 입력하세요.");
+    	return;
+    } else if(info == '' || info == undefined){
+    	alert("정보를 입력하세요.");
+    	return;
+    }
+    
 	$.ajax({
-		url: "/book/sale/updateSale", 
+		url: "/book/sale/update", 
 		method: 'PUT',
 	    dataType: "json",
 		data: {
@@ -268,9 +313,12 @@ function updateBook() {
 			image : image,
 			salePrice : salePrice,
 			info : info
-		}
+		},
+		beforeSend : function(xhr){   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+            xhr.setRequestHeader(header, token);
+        }
 	}).done(function( data ) {
-		if(data.result == success){
+		if(data.result == 'success'){
 			window.alert("중고 책 판매를 수정하였습니다.");
 	        
 			//$("#pop-sale-view").css("display", "none");
@@ -282,19 +330,7 @@ function updateBook() {
 }
 
 //구매
-function buyBook() {
+function createOrder() {
 	
 }
 
-
-function setDefault() {
-	$("#insertTitle").val('');
-	$("#insertPublisher").val('');
-	$("#insertAuthor").val('');
-	$("#insertCostPrice").val('');
-	$("#insertSalePrice").val('');
-	$("#insertRegiDate").val('');
-	$("#insertGenre").val('');
-	$("#insertState").val('');
-	$("#insertInfo").val('');
-}
