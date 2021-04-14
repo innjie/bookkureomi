1d047b3513623315f01858643bc526b97429bb0c
im57 <eovhehd1986@gmail.com>
2021-04-14 17:52:33 +0900

    [add] pop-up (recharge)

--- src/main/resources/static/js/point/point.js
+++ src/main/resources/static/js/point/point.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	listRecharge();
 });
 
-//판매 중고 서적 나열
+//충전/사용 내역 나열
 function listRecharge(){
 	$.ajax({
 		url: "/book/point/list", 
@@ -14,26 +14,45 @@ function listRecharge(){
 		var recharge;
 		
 		if(data.rechargeList.length > 0) {
-			result = "<ul>";
-/*			result = 
-				//	"<table class=\"table-list\">";
-					+ "<colgroup><col width=\"10%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /></colgroup><tbody>"
-					+ "<thead><tr><th>No.</th><th>충전 포인트</th><th>사용 포인트</th><th>충전/사용 날짜</th><th>충전 방법</th><th>총 포인트</th></tr></thead>";
-*/			
+//			result = "<ul>";
+			result =  "<table class=\"table-list\">"
+					+ "<colgroup><col width=\"10%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /><col width=\"15%\" /></colgroup>"
+					+ "<thead><tr><th class=\"table-header\">No.</th><th class=\"table-header\">충전/사용 날짜</th><th class=\"table-header\">충전 포인트</th><th class=\"table-header\">사용 포인트</th><th class=\"table-header\">충전 방법</th><th class=\"table-header\">총 포인트</th></tr></thead><tbody>";
+			
 			
 			for(var i=0; i<data.rechargeList.length; i++){
-				recharge = "<li><h3>" + data.rechargeList[i].rechargeNo + "</h3></li>"
+				result += "<tr>"
+							+ "<td class=\"table-text\">" + (i+1) + "</td>"
+							+ "<td class=\"table-text\">" + data.rechargeList[i].rcDate + "</td>";
+				
+				//충전/사용 구분
+				if(data.rechargeList[i].rcType == "충전"){
+					result += "<td class=\"table-text\">" + data.rechargeList[i].rcPoint + "</td>"
+							+ "<td class=\"table-text\"></td>";
+				} else if(data.rechargeList[i].rcType == "사용") {
+					result += "<td class=\"table-text\"></td>"
+							+ "<td class=\"table-text\">" + data.rechargeList[i].rcPoint + "</td>";
+				}
+
+				
+				result += "<td class=\"table-text\">" + data.rechargeList[i].rcMethod + "</td>"
+							+ "<td class=\"table-text\">" + data.rechargeList[i].totalPoint + "</td>";
+				
+				
+	/*			recharge = "<li><h3>" + data.rechargeList[i].rechargeNo + "</h3></li>"
 					//	+"<li>" + data.rechargeList[i].user.name + "</li>"
 						+"<li>" + data.rechargeList[i].rcPoint + "</li>"
 						+"<li>" + data.rechargeList[i].rcDate + "</li>"
 						+"<li>" + data.rechargeList[i].rcMethod + "</li>"
 						+"<li>" + data.rechargeList[i].rcType + "</li>"
 						+"<li>" + data.rechargeList[i].totalPoint + "</li>";
-				result += recharge;
+				result += recharge;*/
+				
+				result += "</tr>";
 			}
 			
-			result += "</ul>";
-			//result += "</tbody></table>";
+			//result += "</ul>";
+			result += "</tbody></table>";
 		} else {
 			result = "<p class=\"find-nothing\">검색결과가 없습니다.</p>";
 		}
@@ -45,7 +64,7 @@ function listRecharge(){
     });
 }
 
-//판매 중고서적 상세보기
+//포인트 보기
 function detailPoint() {	
 	$.ajax({
 		url: "/book/point/detail", 
@@ -59,4 +78,26 @@ function detailPoint() {
     .fail( function( textStatus ) {
         alert( "Request failed: " + textStatus );
     });
+}
+
+
+function closeCreatePopup() {
+	$("#pop-recharge-create").css("display", "none");
+}
+
+function setDefault() {
+	$("#rechargeType").val('kakao');
+	$("#rechargePrice").val('1000');
+}
+
+//충전
+function createRechargeForm(){
+	setDefault();
+	$("#pop-recharge-create").css("display", "block");
+}
+function createRecharge(){
+	var rechargeType = $("#rechargeType").val();
+	var rechargePrice = $("#rechargePrice").val();
+	alert(rechargeType +" "+ rechargePrice);
+	
 }
