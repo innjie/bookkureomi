4946633a711391d591218af8e43ad16f6603eaa9
im57 <eovhehd1986@gmail.com>
2021-04-18 16:29:53 +0900

    [update] kakaopay

--- src/main/java/com/inyoon/bookkureomi/kakao/KakaoPayService.java
+++ src/main/java/com/inyoon/bookkureomi/kakao/KakaoPayService.java
@@ -22,7 +22,8 @@ public class KakaoPayService {
     private static final String HOST = "https://kapi.kakao.com";
     
     private KakaoPayReadyVO kakaoPayReadyVO;
-   
+    private KakaoPayApprovalVO kakaoPayApprovalVO;
+    
     //public String kakaoPayReady(Sale sale, User user) {
     public String kakaoPayReady(Recharge recharge) {
     	
@@ -44,9 +45,9 @@ public class KakaoPayService {
         params.add("quantity", "1");
         params.add("total_amount", String.valueOf(recharge.getRcPoint()));
         params.add("tax_free_amount", "100");
-        params.add("approval_url", "http://localhost:8000/book/point/rechargePoint?result=success&rcMethod=kakao&rcPoint="+String.valueOf(recharge.getRcPoint()));
-        params.add("cancel_url", "http://localhost:8000/book/point/rechargePoint?result=cancel&rcMethod=kakao&rcPoint="+String.valueOf(recharge.getRcPoint()));
-        params.add("fail_url", "http://localhost:8000/book/point/rechargePoint?result=fail&rcMethod=kakao&rcPoint="+String.valueOf(recharge.getRcPoint()));
+        params.add("approval_url", "http://localhost:8000/book/kakao/success");
+        params.add("cancel_url", "http://localhost:8000/book/kakao/cancel");
+        params.add("fail_url", "http://localhost:8000/book/kakao/fail");
  
         HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
  
@@ -66,6 +67,50 @@ public class KakaoPayService {
         }
         
         return "/";
+    }
+    
+    public KakaoPayApprovalVO kakaoPayInfo(String pg_token) {
+    	 
+       // log.info("KakaoPayInfoVO............................................");
+       // log.info("-----------------------------");
+        
+        RestTemplate restTemplate = new RestTemplate();
+ 
+        // 서버로 요청할 Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "KakaoAK " + "6b3d258606985783d2c11c2a0cefd63d");
+        headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+ 
+        // 서버로 요청할 Body
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.add("cid", "TC0ONETIME");
+        params.add("tid", kakaoPayReadyVO.getTid());
+        //params.add("partner_order_id", recharge.getUser().getUserNo() + "-" + recharge.getRechargeNo());
+        //params.add("partner_user_id", recharge.getUser().getId());
+        //params.add("total_amount", String.valueOf(recharge.getRcPoint()));
+        params.add("partner_order_id", "1-0");
+        params.add("partner_user_id", "im");
+        params.add("total_amount", "1000");
+        params.add("pg_token", pg_token);
+        
+        HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+        
+        try {
+            kakaoPayApprovalVO = restTemplate.postForObject(new URI(HOST + "/v1/payment/approve"), body, KakaoPayApprovalVO.class);
+            System.out.println("" + kakaoPayApprovalVO);
+          
+            return kakaoPayApprovalVO;
+        
+        } catch (RestClientException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return null;
     }
 }
  
--- src/main/java/com/inyoon/bookkureomi/point/PointController.java
+++ src/main/java/com/inyoon/bookkureomi/point/PointController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.inyoon.bookkureomi.domain.Recharge;
 import com.inyoon.bookkureomi.domain.User;
+import com.inyoon.bookkureomi.kakao.KakaoPayService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -32,6 +33,8 @@ public class PointController {
 	
 	@Autowired
 	private PointService pointService;
+	@Autowired
+    private KakaoPayService kakaoPayService;
 	
 	@ApiOperation(value="포인트 확인 화면 이동", notes="포인트 확인 화면으로 이동한다.")
 	@GetMapping("/point/view")
@@ -45,8 +48,12 @@ public class PointController {
 	@GetMapping("/point/detail")
 	public Map<String, Object> detailPoint() throws Exception {
 
-		//int point = pointService.checkPointuserNo1);
-		int point = pointService.checkPoint(1);
+		//int point = pointService.checkPoint(userNo1);
+		int point = 0;
+		
+		if(pointService.checkHasPoint(1) != 0) {
+			point = pointService.checkPoint(1);
+		}
 		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("point", point);
@@ -70,84 +77,58 @@ public class PointController {
     }
 	
 	@ApiOperation(value="포인트 충전", notes="포인트를 충전한다.")
-	//@ResponseBody //@RestController 시 생략 가능
-	@GetMapping("/point/rechargePoint")
-	public ModelAndView rechargePoint(
-			@RequestParam("rcMethod") String rcMethod,
-			@RequestParam("rcPoint") String rcPoint,
-			@RequestParam(value="result", required=false) String result) throws Exception {
+	@ResponseBody //@RestController 시 생략 가능
+	@GetMapping("/point/create")
+	public Map<String, Object> rechargePoint(
+			@RequestParam("rcPoint") String rcPoint) throws Exception {
 		
 		Map<String, Object> map = new HashMap<String, Object>();
-		ModelMap model = new ModelMap();
 
-		if(result.equals("success")) {
-			User user = new User();
-			user.setUserNo(1);
-			
-			Recharge recharge = new Recharge();
-			//recharge.setRechargeNo(pointService.getRechargeNo(userNo));
-			//recharge.setTotalPoint(pointService.checkPoint(userNo) + Integer.parseInt(rcPoint));
-			recharge.setRechargeNo(pointService.getRechargeNo(1));
-			recharge.setTotalPoint(pointService.checkPoint(1) + Integer.parseInt(rcPoint));
-			recharge.setRcType("충전");
-			recharge.setRcMethod(rcMethod);
-			recharge.setRcPoint(Integer.parseInt(rcPoint));
-			recharge.setUser(user);
-			
-			pointService.rechargePoint(recharge);
-		}
+		User user = new User();
+		user.setUserNo(1);
 		
-		map.put("result", result);
-		model.put("result", result);
+		Recharge recharge = new Recharge();
+		//recharge.setRechargeNo(pointService.getRechargeNo(userNo));
+		//recharge.setTotalPoint(pointService.checkPoint(userNo) + Integer.parseInt(rcPoint));
+		recharge.setRechargeNo(pointService.getRechargeNo(1));
+		recharge.setTotalPoint(pointService.checkPoint(1) + Integer.parseInt(rcPoint));
+		recharge.setRcType("충전");
+		recharge.setRcMethod("kakao");
+		recharge.setRcPoint(Integer.parseInt(rcPoint));
+		recharge.setUser(user);
 		
-        return new ModelAndView("point/point", model);
+		pointService.rechargePoint(recharge);
+				
+        return map;
 	}
 	
-/*
-	//포인트 정보 보기
-	//get point detail
-	@RequestMapping("/recharge/getPoint.do")
-	public ModelAndView getPoint(
-			@RequestParam("orderNo") int orderNo) throws Exception {
-		//
-		//checkPoint(int userNo);
-		//getRechargeList(int userNo);
-	}
+	@ApiOperation(value="포인트 충전 성공", notes="포인트 충전에 성공한다.")
+	@GetMapping("/kakao/success")
+    public ModelAndView kakaoPaySuccess(
+    		@RequestParam("pg_token") String pg_token,
+    		ModelMap model) {
+		model.put("result", "success");
 
-	//카카오페이 포인트 충전 폼
-	//recharge point form by kakaoPay
-	@RequestMapping("/point/rechargePointKakao.do", method=RequestMethod.GET)
-	public String rechargForm(
-			) throws Exception {
-		//
+		model.put("info", kakaoPayService.kakaoPayInfo(pg_token));
+		
+		return new ModelAndView("point/result", model);
 	}
 	
-	//카카오페이 포인트 충전
-	//recharge point by kakaoPay
-	@RequestMapping("/point/rechargePointKakao.do", method=RequestMethod.POST)
-	public String  rechargePoint(
-			@RequestParam("rcPoint") int rcPoint,
-			HttpServletRequest request,
-			ModelMap model) throws Exception {
-		//
-	}
+	@ApiOperation(value="포인트 충전 취소", notes="포인트 충전을 취소한다.")
+	@GetMapping("/kakao/cancel")
+    public ModelAndView kakaoPayCancel(
+    		ModelMap model) {
+		model.put("result", "cancel");
 
-	//포인트 충전 폼
-		//recharge point form
-	@RequestMapping("/point/rechargePoint.do", method=RequestMethod.GET)
-	public String rechargForm(
-				) throws Exception {
-		//
+		return new ModelAndView("point/result", model);
 	}
 	
-	//포인트 충전
-		//recharge point
-	@RequestMapping("/point/rechargePoint.do", method=RequestMethod.POST)
-	public String  rechargePoint(
-			@RequestParam("rcPoint") int rcPoint,
-			HttpServletRequest request,
-			ModelMap model) throws Exception {
-		//
+	@ApiOperation(value="포인트 충전 실패", notes="포인트 충전에 실패한다.")
+	@GetMapping("/kakao/fail")
+    public ModelAndView kakaoPayFail(
+    		ModelMap model) {
+		model.put("result", "fail");
+
+		return new ModelAndView("point/result", model); 	
 	}
-*/
 }
